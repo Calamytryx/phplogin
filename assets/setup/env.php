3a5c6de60c083ef9e1c2a19704ab6e2ccbf1dd55
Calamytryx <91600814+Calamytryx@users.noreply.github.com>
2023-11-07 19:20:52 +0800

    Update env.php

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -1,35 +1,15 @@
 <?php
 
-if (!defined('APP_NAME'))
-  define('APP_NAME', 'VTUBERS');
-if (!defined('APP_ORGANIZATION'))
-  define('APP_ORGANIZATION', 'Mtrx');
-if (!defined('APP_OWNER'))
-  define('APP_OWNER', 'Calamytryx');
-if (!defined('APP_DESCRIPTION'))
-  define('APP_DESCRIPTION', 'vtuber social network');
 
-if (!defined('ALLOWED_INACTIVITY_TIME'))
-  define('ALLOWED_INACTIVITY_TIME', time() + 1 * 60);
+if (!defined('DB_DATABASE'))        define('DB_DATABASE', 'klik_loginsystem');
+if (!defined('DB_HOST'))            define('DB_HOST','127.0.0.1');
+if (!defined('DB_USERNAME'))        define('DB_USERNAME','root');
+if (!defined('DB_PASSWORD'))        define('DB_PASSWORD' ,'');
+if (!defined('DB_PORT'))            define('DB_PORT' ,'');
 
-if (!defined('DB_DATABASE'))
-  define('DB_DATABASE', 'database_mtrx');
-if (!defined('DB_HOST'))
-  define('DB_HOST', 'localhost');
-if (!defined('DB_USERNAME'))
-  define('DB_USERNAME', 'Calamyty');
-if (!defined('DB_PASSWORD'))
-  define('DB_PASSWORD', 'Mytryx');
-if (!defined('DB_PORT'))
-  define('DB_PORT', '3006');
 
-if (!defined('MAIL_HOST'))
-  define('MAIL_HOST', 'smtp.gmail.com');
-if (!defined('MAIL_USERNAME'))
-  define('MAIL_USERNAME', 'ton.agustin09@gmail.com');
-if (!defined('MAIL_PASSWORD'))
-  define('MAIL_PASSWORD', 'apjo omwz jgwp zxws');
-if (!defined('MAIL_ENCRYPTION'))
-  define('MAIL_ENCRYPTION', 'ssl');
-if (!defined('MAIL_PORT'))
-  define('MAIL_PORT', 465);
+if (!defined('MAIL_HOST'))          define('MAIL_HOST', 'smtp.gmail.com');
+if (!defined('MAIL_USERNAME'))      define('MAIL_USERNAME', 'example@email.com');
+if (!defined('MAIL_PASSWORD'))      define('MAIL_PASSWORD', 'example-password');
+if (!defined('MAIL_ENCRYPTION'))    define('MAIL_ENCRYPTION', 'ssl');
+if (!defined('MAIL_PORT'))          define('MAIL_PORT', 465);
