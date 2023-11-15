0c4ca3f8e598486936127c965e08f59274dbe6ce
Calamytryx <91600814+Calamytryx@users.noreply.github.com>
2023-11-15 21:19:53 +0800

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,11 +1,11 @@
 <?php
-define('TITLE', "Home");
+define('TITLE', "Dashboard");
 include '../assets/layouts/header.php';
 check_verified();
 ?>
 
 <?php if ($_SESSION['user_level'] == "3") { ?>
-  <?php include("includes/admin-mod/admin.php") ?>
+  <?php include("includes/admin-mod/admin.php"); ?>
 <?php } elseif ($_SESSION['user_level'] == "2") { ?>
   <?php include("includes/admin-mod/mod.php") ?>
 <?php } else { ?>
